d750022d7ec66985605863706ce03dee99c5f5d6
Sergey Prilukin <sprilukin@gmail.com>
2017-02-06 00:39:33 +0200

    first success: autowatch works.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,8 +4,16 @@ module.exports = {
     entry: {
         testModule: './src/testModule.js'
     },
+
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, "..", 'backend', "public", "javascripts")
-    }
+    },
+
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: "source-map"
 };
