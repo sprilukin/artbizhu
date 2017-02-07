03955f853e6b5145e071f6dc004df71e2e6ce142
Sergey Prilukin <sprilukin@gmail.com>
2017-02-07 09:59:38 +0200

    added babel

--- frontend/testModule.js
+++ frontend/testModule.js
@@ -1,3 +1,5 @@
 let a = require("./anotherModule");
 
+fetch('/users.html').then(() => console.log(arguments));
+
 console.log(a);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-let path = require('path');
+let path = require('path'),
+    webpack = require("webpack");
 
 module.exports = {
     entry: {
-        testModule: './frontend/testModule.js'
+        testModule: 'testModule.js'
     },
 
     output: {
@@ -10,10 +11,53 @@ module.exports = {
         path: path.resolve(__dirname, "public", "javascripts")
     },
 
-    watch: true,
-    watchOptions: {
-        aggregateTimeout: 100
+    resolve: {
+        modules: [
+            path.join(__dirname, "frontend"),
+            "node_modules"
+        ]
     },
+
+    module: {
+        rules: [
+            /*{
+                test: require.resolve("whatwg-fetch"),
+                use: 'imports?this=>global!exports?fetch=global.fetch'
+            },*/
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['es2017'],
+                            plugins: ["transform-runtime"]
+                        }
+                    }
+                ],
+                exclude: [/node_modules/]
+            }
+        ]
+    },
+
+    plugins: [
+        // new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            // 'Promise': 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            // 'fetch': 'whatwg-fetch'
+        }),
+        //new webpack.optimize.CommonsChunkPlugin('app', null, false),
+        // new webpack.NoEmitOnErrorsPlugin(),
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve('./', 'index.html'),
+        //     webpackDevServer: '/webpack-dev-server.js'
+        // })
+    ],
+
+    // watch: true,
+    // watchOptions: {
+    //     aggregateTimeout: 100
+    // },
 
     devtool: "source-map"
 };
