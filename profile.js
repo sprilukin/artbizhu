380d8976676db151bd9c80b764580764465daaa5
Sergey Prilukin <sprilukin@gmail.com>
2017-02-09 12:57:19 +0200

    changed profile resultion to the following: env -> <[profile].json | .profile.json> ->  development.json -> default.json

--- profile.js
+++ profile.js
@@ -1,15 +1,15 @@
 /* eslint no-console: "off" */
 
 // profile resultion precedence (from high to low):
-// env -> .profile.json -> [profile].json -> default.json
-// where [profile] resolved from NODE_ENV or is "development" by default
+// env -> <[profile].json | .profile.json> ->  development.json -> default.json
+// where [profile] resolved from NODE_ENV
 
 let fs = require("fs"),
     profiles = require("./profiles"),
     environmentProfile = require("./profiles/environment");
 
 // get profile name from NODE_ENV
-let profile = profiles[process.env.NODE_ENV] || profiles.development;
+let profile = profiles.development;
 
 const localProfilePath = "./.profile.json";
 let isProfileFileExists = fs.existsSync(localProfilePath);
@@ -21,6 +21,8 @@ if (isProfileFileExists) {
         console.log("Invalid './.profile.json' file");
     }
 }
+
+profile = profiles[process.env.NODE_ENV] || profile;
 
 profile = Object.assign(profile, environmentProfile);
 
