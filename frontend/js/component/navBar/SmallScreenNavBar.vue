ec02de089e53cccf184a564baaa95b8d5905200c
Sergey Prilukin <sprilukin@gmail.com>
2017-02-20 00:21:52 +0200

    Small menu icon

--- frontend/js/component/navBar/SmallScreenNavBar.vue
+++ frontend/js/component/navBar/SmallScreenNavBar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="uk-hidden@m uk-margin">
-        <button class="uk-button uk-button-default" type="button">Меню</button>
+        <button class="uk-button uk-button-default" type="button">
+            <span class="uk-icon uk-margin-small-right" href="#" uk-icon="icon: menu; ratio: 2"></span>
+        </button>
         <div uk-dropdown>
             <ul class="uk-nav uk-dropdown-nav">
                 <template v-for="menuItem in menuItems">
