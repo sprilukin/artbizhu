78e5e510c20eadf5a009170c99c144ed531641a1
Sergey Prilukin <sprilukin@gmail.com>
2017-02-21 23:31:53 +0200

    Added 404 page

--- frontend/js/component/NotFound.vue
+++ frontend/js/component/NotFound.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-        <div>404 page not found</div>
-        <router-link to="/">Go to Index</router-link>
-    </div>
+    <mainLayout>
+        <div class="uk-card-large uk-grid-match uk-margin uk-flex-center" uk-grid>
+            <div class="uk-card uk-card-body uk-text-center">
+                <h3 class="uk-card-title">404</h3>
+                <p>Страница не найдена</p>
+                <p><router-link to="/">На главную</router-link></p>
+            </div>
+        </div>
+    </mainLayout>
 </template>
+
+<script>
+    import MainLayout from "./layout/MainLayout.vue";
+
+    export default {
+        components: {
+            mainLayout: MainLayout
+        }
+    }
+</script>
