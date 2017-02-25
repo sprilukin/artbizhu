ed26e250a28902c552fddcf9c34067379d641b20
Sergey Prilukin <sprilukin@gmail.com>
2017-02-25 12:18:24 +0200

    moved few components to common components

---
+++ frontend/js/common/component/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="uk-card-large uk-grid-match uk-margin uk-flex-center" uk-grid>
+        <div class="uk-card uk-card-body uk-text-center">
+            <h3 class="uk-card-title">404</h3>
+            <p>Страница не найдена</p>
+            <p><router-link to="/">На главную</router-link></p>
+        </div>
+    </div>
+</template>
--- frontend/js/public/component/NotFound.vue
+++ frontend/js/public/component/NotFound.vue
@@ -1,21 +1,17 @@
 <template>
     <mainLayout>
-        <div class="uk-card-large uk-grid-match uk-margin uk-flex-center" uk-grid>
-            <div class="uk-card uk-card-body uk-text-center">
-                <h3 class="uk-card-title">404</h3>
-                <p>Страница не найдена</p>
-                <p><router-link to="/">На главную</router-link></p>
-            </div>
-        </div>
+        <notFound></notFound>
     </mainLayout>
 </template>
 
 <script>
     import MainLayout from "./layout/MainLayout.vue";
+    import NotFound from "uicommon/component/NotFound.vue";
 
     export default {
         components: {
-            mainLayout: MainLayout
+            mainLayout: MainLayout,
+            notFound: NotFound
         }
     }
 </script>
