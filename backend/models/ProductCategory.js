0f50b9c377c1078d5f9d7676293fa1644b371cf8
Sergey Prilukin <sprilukin@gmail.com>
2017-02-28 22:22:32 +0200

    Added images to product category

--- backend/models/ProductCategory.js
+++ backend/models/ProductCategory.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const ProductCategory = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
     name: String,
     description: String,
+    images: [
+        {
+            _id: false,
+            uri: String
+        }
+    ],
     updated_at: {
         type: Date,
         default: Date.now
